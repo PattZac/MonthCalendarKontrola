6e03963a81e6746a22cbcdab94cca9174a9cb920
Pat <113007904+PattZac@users.noreply.github.com>
2022-10-23 15:25:41 +0200

    Add files via upload

---
+++ MonthCalendarKontrola/MonthCalendarKontrola/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonthCalendarKontrola
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TimeSpan datum = monthCalendar1.SelectionEnd - monthCalendar1.SelectionStart;
+            if (datum.Days < 1) {
+                MessageBox.Show("Korištenjem tipke SHIFT odaberite opseg datuma.","Oprez");
+            } 
+            else {
+                MessageBox.Show("Rezervirali ste usluge u trajanju od "+ datum.Days.ToString() + " dana.", "Rezervacija");
+                MessageBox.Show("Vasa rezervacija počinje " + monthCalendar1.SelectionStart.ToShortDateString() + " a završava se " + monthCalendar1.SelectionEnd.ToShortDateString() + ".", "Rezervacija");
+            }
+        }
+    }
+}
